83841b30826e9856bc4d9649c5271fea749432a7
Thomas <89458019+ThomasMrs@users.noreply.github.com>
2025-08-19 10:09:00 +0200

    static site

--- bronzette-au-village/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { BronzettePageComponent } from './pages/bronzette.page';
-
-export const routes: Routes = [
-  { path: '', component: BronzettePageComponent },
-  { path: '**', redirectTo: '' }
-];
